5417f34ca26640797b6036e6caf23e78303c3a70
teemodisi <teemodisi@gmail.com>
2025-11-05 02:26:36 +0800

    temp

--- DuckController..cs
+++ DuckController..cs
@@ -1,15 +1,52 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace DuckovController
 {
-    public class DuckovController : Duckov.Modding.ModBehaviour
+    public class ModBehaviour : Duckov.Modding.ModBehaviour
     {
         private void Awake()
         {
             Debug.Log($"[{nameof(DuckovController)}] Duckov Controller is loaded.");
-            
-            
         }
+
+        protected override void OnAfterSetup()
+        {
+ 
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F2))
+            {
+                ModTest();
+            }
+        }
+
+        private void ModTest()
+        {
+            var objs = SceneManager.GetActiveScene().GetRootGameObjects();
+            var mainMenu =  GameObject.FindFirstObjectByType<MainMenu>();
+
+            // mainMenu.
+           
+            // var coms = mainMenu.GetComponents<Component>();
+            // foreach (var o in coms)
+            // {
+            //     Debug.Log($"{o.GetInstanceID()} {o.name} {o.GetType()}");
+            // }
+
+            // var menu = FindObjectOfType<Menu>();
+            // Debug.Log("1");
+            // Debug.Log(menu == null);
+            // var items = typeof(Menu).GetField("items").GetValue(menu) as HashSet<MenuItem>;
+            // Debug.Log("2");
+            // Debug.Log(items?.Count);
+        }
+
+        protected override void OnBeforeDeactivate() { }
     }
 }
---
+++ Utils.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace DuckovController
+{
+    public class Utils
+    {
+        public void ShowAllComponents(Transform transform)
+        {
+            var stack = new Stack<Transform>();
+            stack.Push(transform);
+            var level = 0;
+            var sb = new StringBuilder();
+            while (stack.Count > 0)
+            {
+                var cur = stack.Pop();
+                if (cur != null)
+                {
+                    sb.Clear();
+                    for (var i = 0; i < level; i++)
+                    {
+                        sb.Append("    ");
+                    }
+                    // sb.Append()
+                    // Debug.Log($"{string.}");
+
+                    level++;
+                    var count = cur.childCount;
+                    for (var i = 0; i < count; i++)
+                    {
+                        var obj = cur.GetChild(i);
+                        // stack.Push();
+                    }
+                }
+            }
+        }
+    }
+}
