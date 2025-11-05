be6d8d9638246b3b20ed48396b2dea820057c922
Teemodisi <teemodisi@gmail.com>
2025-11-05 11:57:19 +0800

    mvProject

--- DuckovController/DuckController..cs
+++ DuckovController/DuckController..cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Duckov.UI;
-using UI;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace DuckovController
@@ -14,17 +10,12 @@ namespace DuckovController
             Debug.Log($"[{nameof(DuckovController)}] Duckov Controller is loaded.");
         }
 
-        protected override void OnAfterSetup()
-        {
- 
-        }
-
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.F2))
             {
-                var canvas =  FindFirstObjectByType<Canvas>();
-                Utils.ShowAllComponents(canvas.transform,showCom: true);
+                var canvas = FindFirstObjectByType<Canvas>();
+                Utils.ShowAllComponents(canvas.transform, showCom: true);
             }
             if (Input.GetKeyDown(KeyCode.F3))
             {
@@ -32,13 +23,13 @@ namespace DuckovController
             }
         }
 
+        protected override void OnAfterSetup() { }
+
         private void ModTest()
         {
             var objs = SceneManager.GetActiveScene().GetRootGameObjects();
 
-
             // View.ActiveView
-
 
             // var coms = mainMenu.GetComponents<Component>();
             // foreach (var o in coms)
