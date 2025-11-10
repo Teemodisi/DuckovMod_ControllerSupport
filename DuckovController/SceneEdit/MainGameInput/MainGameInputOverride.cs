12b47b2459b823a67d5b6e3c7b3eb8348b7f6cb3
teemodisi <teemodisi@gmail.com>
2025-11-11 03:03:14 +0800

    右键瞄准映射，摇杆方案更新

--- DuckovController/SceneEdit/MainGameInput/MainGameInputOverride.cs
+++ DuckovController/SceneEdit/MainGameInput/MainGameInputOverride.cs
@@ -12,17 +12,32 @@ namespace DuckovController.SceneEdit.MainGameInput
     {
         private const float aim_smooth_time = 0.1f;
 
+        private const float aim_translation = 20f;
+
         private Vector2 _aimSmoothVel = Vector2.zero;
 
         private Dictionary<InputAction, Action<InputAction.CallbackContext>> _bindingMap;
 
         private bool _hadBind;
 
+        private Vector2 _lastDirection;
+
         private Vector2 _controllerDirection;
 
         private bool _isGaming;
 
+        private bool _triggerIsDown;
+
+        private bool _isAiming;
+
+        //右摇杆平移模式，用于压枪或者是瞄准
+        private bool IsTranslateMod => _isAiming || _triggerIsDown;
+
         private InputManager InputManager => CharacterInputControl.Instance.inputManager;
+
+        private Vector2 AxisCenter => new Vector2(Screen.width / 2f, Screen.height / 2f);
+
+        private float AimDirectDistance => Mathf.Min(Screen.width, Screen.height) * 0.4f;
 
         private void Awake()
         {
@@ -33,12 +48,9 @@ namespace DuckovController.SceneEdit.MainGameInput
                 { GamePadInput.Instance.ReloadAction, CharacterInputControl.Instance.OnReloadInput },
                 { GamePadInput.Instance.InteractAction, CharacterInputControl.Instance.OnInteractInput },
                 { GamePadInput.Instance.MovementAction, CharacterInputControl.Instance.OnPlayerMoveInput },
-                {
-                    GamePadInput.Instance.TriggerAction,
-                    CharacterInputControl.Instance.OnPlayerTriggerInputUsingMouseKeyboard
-                },
-                { GamePadInput.Instance.AimAction, OnAim },
-                { GamePadInput.Instance.AimDirectionAction, OnAimDirection },
+                { GamePadInput.Instance.TriggerAction, OnTriggerInput },
+                { GamePadInput.Instance.AimAction, OnAimInput },
+                { GamePadInput.Instance.AimDirectionAction, OnAimDirectionInput },
                 { GamePadInput.Instance.QuackAction, CharacterInputControl.Instance.OnQuackInput }
             };
             View.OnActiveViewChanged += OnActiveViewChanged;
@@ -51,17 +63,23 @@ namespace DuckovController.SceneEdit.MainGameInput
             {
                 return;
             }
-            var w = Screen.width;
-            var h = Screen.height;
-            var center = new Vector2(w / 2f, h / 2f);
-            //TODO：整体缺少偏移
-            var controllerTarget = center + _controllerDirection.normalized * (Mathf.Min(w, h) * 0.4f);
-            var cur = InputManager.MousePos;
-            var next = Vector2.SmoothDamp(cur, controllerTarget, ref _aimSmoothVel, aim_smooth_time);
-            var delta = next - cur;
-            if (delta.magnitude > 0.01)
+            if (IsTranslateMod)
             {
-                InputManager.SetAimInputUsingMouse(delta);
+                //改为平移
+                InputManager.SetAimInputUsingMouse(_controllerDirection * aim_translation);
+                _lastDirection = (InputManager.MousePos - AxisCenter).normalized;
+            }
+            else
+            {
+                //TODO：整体缺少偏移
+                var controllerTarget = AxisCenter + _lastDirection.normalized * AimDirectDistance;
+                var cur = InputManager.MousePos;
+                var next = Vector2.SmoothDamp(cur, controllerTarget, ref _aimSmoothVel, aim_smooth_time);
+                var delta = next - cur;
+                if (delta.magnitude > 0.01f)
+                {
+                    InputManager.SetAimInputUsingMouse(delta);
+                }
             }
         }
 
@@ -96,19 +114,43 @@ namespace DuckovController.SceneEdit.MainGameInput
             _isGaming = View.ActiveView == null;
         }
 
-        private void OnAimDirection(InputAction.CallbackContext context)
+        private void OnAimDirectionInput(InputAction.CallbackContext context)
         {
             if (context.performed)
             {
                 _controllerDirection = context.ReadValue<Vector2>();
+                _lastDirection = _controllerDirection;
+            }
+            if (context.canceled)
+            {
+                _controllerDirection = Vector2.zero;
             }
         }
 
-        private void OnAim(InputAction.CallbackContext context)
+        private void OnTriggerInput(InputAction.CallbackContext context)
         {
-            if (context.started) { }
-            if (context.canceled) { }
-            // CharacterInputControl.Instance.inputManager.SetAimType(AimTypes.normalAim);
+            CharacterInputControl.Instance.OnPlayerTriggerInputUsingMouseKeyboard(context);
+            if (context.started)
+            {
+                _triggerIsDown = true;
+            }
+            if (context.canceled)
+            {
+                _triggerIsDown = false;
+            }
+        }
+
+        private void OnAimInput(InputAction.CallbackContext context)
+        {
+            CharacterInputControl.Instance.OnPlayerAdsInput(context);
+            if (context.started)
+            {
+                _isAiming = true;
+            }
+            if (context.canceled)
+            {
+                _isAiming = false;
+            }
         }
 
     #region Reflection
@@ -118,15 +160,6 @@ namespace DuckovController.SceneEdit.MainGameInput
 
         private GameCamera.CameraAimingTypes CameraAimingType =>
             (GameCamera.CameraAimingTypes)GameCameraAimingTypeMethodInfo.GetValue(GameCamera.Instance);
-
-        private FieldInfo CharacterInputControlMousePosMethodInfo { get; } = typeof(CharacterInputControl)
-            .GetField("mousePos", BindingFlags.NonPublic | BindingFlags.Instance);
-
-        private Vector2 PlayerMousePos
-        {
-            get => (Vector2)CharacterInputControlMousePosMethodInfo.GetValue(CharacterInputControl.Instance);
-            set => CharacterInputControlMousePosMethodInfo.SetValue(CharacterInputControl.Instance, value);
-        }
 
     #endregion
     }
